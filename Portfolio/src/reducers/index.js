d241f01df5ed0030b2c7cade548b53fb19506efd
Andry Mironov <andry.mironov94@gmail.com>
2019-03-07 03:30:07 +0200

    Translator

--- Portfolio/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import todos from './projects';
-
-const allReducers = combineReducers({
-   projects:todos   //** Передаем в projects весь массив  из ./projects */
-});
-
-export default allReducers;
